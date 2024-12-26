205ac094e6d95403eb647a0cbb7bf34e0d983865
tn-junior <juju2004.tary@gmail.com>
2024-12-25 21:46:03 -0300

    fix: adjusting stylization

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import "../../styles/_app";
 
 const HeroSection: React.FC = () => {
   const router = useRouter();
